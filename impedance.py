880131e07586ddf409684390243b26ebc9970436
Per Hansson Adrian <per.hansson80@gmail.com>
2017-06-05 19:54:58 -0700

    adding circuit components

--- impedance.py
+++ impedance.py
@@ -10,7 +10,7 @@ def parallel(Z1,Z2):
 def series(Z1,Z2):
     return Z1 + Z2
 
-class Component:
+class Component(object):
     def __init__(self, value, name=''):
         self.value = value
         self.name = name
@@ -19,14 +19,14 @@ class Component:
 
 class Capacitor(Component):
     def Z(self,f):
-        return 1.0/complex(0,2*pi*f*self.value)
+        return complex(0,-1.0/(2*pi*f*self.value))
 
 class Resistor(Component):
     def Z(self,f):
         return complex(self.value,0)
 
 
-class Circuit:
+class Circuit(object):
 
     """ Base class. """
 
@@ -38,18 +38,55 @@ class Circuit:
         """Should override this function"""
 
 
+class HEMTCircuit(Circuit):
+    def __init__(self,name, Rg=1e12, Cgs=100e-12):
+        super(HEMTCircuit, self).__init__(name)
+        self.Rg = Resistor(Rg,'Rg')
+        self.Cgs = Capacitor(Cgs,'Cgs)')
+        
+    def Z_tot(self, freq):
+        # gate to source resistance and gate to source capacitance in parallel
+        Zt = parallel(self.Rg.Z(freq), self.Cgs.Z(freq))
+        return Zt
+    
+
+class OpAmp(Circuit):
+    def __init__(self,name, Rfb=1e20, Cfb=20e-12):
+        super(OpAmp, self).__init__(name)
+        self.Rfb = Resistor(Rfb,'Rfb')
+        self.Cfb = Capacitor(Cfb,'Cfb)')
+
+    def Z_fb(self, freq):
+        Z = parallel(self.Rfb.Z(freq), self.Cfb.Z(freq))
+        return Z
+
+    def Z_tot(self, freq):
+        return 0
+    
+
+        
+
+
 class ChargeCircuit(Circuit):
 
     """Description of charge circuit for ionization readout for SCDMS."""
 
-    def __init__(self, name, Cdet=200.0e-12, Rbias=100e6, Rbleed=100e6, Cc=10e-9):
+    def __init__(self, name, Cdet=200.0e-12, Rbias=100e6, Rbleed=100e6, Cc=10e-9, Ccg=10e-12, Cfb=1e-12, Rfb=400e6, HEMT=None, opamp=None):
         """init class"""
-        self.name = name
-        self.Cc = Capacitor(Cc, 'Cc')
+        super(ChargeCircuit, self).__init__(name)
         self.Cdet = Capacitor(Cdet, 'Cdet')
         self.Rbias = Resistor(Rbias, 'Rbias')
         self.Rbleed = Resistor(Rbleed, 'Rbleed')
-        
+        # coupling capacitor to the HEMT gate at 4K
+        self.Ccg = Capacitor(Ccg, 'Ccg')
+        # coupling capacitor to the coax wire at MC stage
+        self.Cc = Capacitor(Cc, 'Cc')
+        self.Cfb = Capacitor(Cfb, 'Cfb')
+        self.Rfb = Resistor(Rfb, 'Rfb')
+        if not opamp:
+            self.opamp = OpAmp('OpAmp')
+        if not HEMT:
+            self.HEMT = HEMTCircuit('HEMT')
     
     def Z_MC1(self, freq):
         """Impendance of circuitry at on lower coax PCB at MC stage."""
@@ -64,12 +101,58 @@ class ChargeCircuit(Circuit):
         Z3 = parallel(Z2,self.Rbleed.Z(freq))
 
         return Z3
-        
-        
+
+    def Z1_tot(self,freq):
+        """Impedance from MC stage and HEMT."""
+        # impedance at 4K
+        Z1_4K = series(self.Z_MC1(freq), self.Ccg.Z(freq))
+        # in parallel to HEMT impedance
+        Z1 = parallel(self.Z_HEMT(freq),Z1_4K)
+        return Z1
+
+    def Z_HEMT(self, freq):
+        """Impedance of HEMT circuit"""
+        return self.HEMT.Z_tot(freq)
+
+    def Z_fb(self, freq):
+        """Impedance of feedback cap and resistor network."""
+        Z = parallel(self.Cfb.Z(freq), self.Rfb.Z(freq))
+        return Z
+
+    def Z6(self, freq):
+        """Impedance of opamp feedback network."""
+        Z = self.opamp.Z_fb(freq)
+        return Z
+
+    def Z5(self, freq):
+        """Impedance of opamp comparator bias network."""
+        C118 = Capacitor(100e-9,'C118')
+        R115 = Resistor(3.3e3,'R115')
+        R114 = Resistor(3.3e3,'R114')
+        R1140 = Resistor(1e3,'R1140')
+        Z1 = parallel(R114.Z(freq),R115.Z(freq))
+        Z2 = parallel(Z1,C118.Z(freq))
+        Z = series(R1140.Z(freq), Z2)
+        return Z3
+
+
+    def Z3(self, freq):
+        """Impedance of pole cancellation network."""
+        C1120 = Capacitor(10e-9,'C1120')
+        R113 = Resistor(1.6e3,'R113')
+        R1110 = Resistor(1e2,'R1110')
+        Z1 = series(R1110.Z(freq), C1120.Z(freq))
+        Z = parallel(Z1, R113.Z(freq))
+        return Z
+
+
+    def Z_BJT
+
+
     def Z_tot(self, freq):
-        return self.Z_MC1(freq)
+        return 0.
 
 
-
+    
 
     
--- noise_model.py
+++ noise_model.py
@@ -15,38 +15,73 @@ def get_args():
     parser = argparse.ArgumentParser('Noise Model')
     parser.add_argument('--debug',action='store_true',help='debug toggle')
     parser.add_argument('--noshow',action='store_true',help='Do not show plots.')
+    parser.add_argument('--logy',action='store_true',help='Log y axis.')
     a =parser.parse_args()
     print(a)
     return a
+
+
+
+def to_np_array(func, arr):
+    return np.array([func(a) for a in arr])
+
+
+def impedancePlot(fig_MC211, f, Z, name=None):
+    """Plot impedance and phase"""
+
+    
+    ax_MC211 = plt.subplot(211)
+    plt.plot(f, np.abs(Z))
+    plt.ylabel('Impedance')
+    plt.xlabel('Frequency')
+    if args.logy:
+        ax_MC211.set_yscale(u'log')
+    ax_MC211.set_xscale(u'log')
+
+    Z_phase = to_np_array(cmath.phase, Z)
+
+    ax_MC212 = plt.subplot(212)
+    plt.plot(f, radToDeg(Z_phase))
+    plt.ylabel('Phase')
+    plt.xlabel('Frequency')
+    #ax_MC1.set_yscale(u'log')
+    ax_MC212.set_xscale(u'log')
+
+    if name:
+        plt.savefig(name,bbox_inches='tight')
+
+    return [ax_MC211, ax_MC212]
 
 def main():
 
 
     Q = ChargeCircuit('Circuit')
 
-
     f_arr = np.logspace(0,5)
 
-    Z_MC1 = np.array([Q.Z_MC1(f) for f in f_arr])
-    Z_MC1_phase = np.array([cmath.phase(z) for z in Z_MC1])
-    print('Z_MC1 = ' + str(Z_MC1))
-    print('Z_MC1_phase = ' + str(Z_MC1_phase))
+    Z_MC1 = to_np_array(Q.Z_MC1, f_arr)
+    impedancePlot(plt.figure(1), f_arr, Z_MC1, 'Z_MC1.png')
 
-    fig_MC211 =  plt.figure(1)
-    ax_MC211 = plt.subplot(211)
-    plt.plot(f_arr, np.abs(Z_MC1))
-    plt.ylabel('Impedance')
-    plt.xlabel('Frequency')
-    #ax_MC1.set_yscale(u'log')
-    ax_MC211.set_xscale(u'log')
-    
-    ax_MC212 = plt.subplot(212)
-    plt.plot(f_arr, radToDeg(Z_MC1_phase))
-    plt.ylabel('Phase')
-    plt.xlabel('Frequency')
-    #ax_MC1.set_yscale(u'log')
-    ax_MC212.set_xscale(u'log')
-    plt.savefig('Z_MC1.png',bbox_inches='tight')
+
+    Z_HEMT = to_np_array(Q.Z_HEMT, f_arr)    
+    impedancePlot(plt.figure(2), f_arr, Z_HEMT,'Z_HEMT.png')
+
+
+    Z1_tot = to_np_array(Q.Z1_tot, f_arr)    
+    impedancePlot(plt.figure(3), f_arr, Z1_tot,'Z1_tot.png')
+
+    Z_fb = to_np_array(Q.Z_fb, f_arr)    
+    impedancePlot(plt.figure(4), f_arr, Z_fb,'Z_fb.png')
+
+    Z6 = to_np_array(Q.Z6, f_arr)    
+    impedancePlot(plt.figure(5), f_arr, Z6,'Z6.png')
+
+    Z5 = to_np_array(Q.Z5, f_arr)    
+    impedancePlot(plt.figure(6), f_arr, Z5,'Z5.png')
+
+    Z3 = to_np_array(Q.Z3, f_arr)    
+    impedancePlot(plt.figure(7), f_arr, Z3,'Z3.png')
+
     
     if not args.noshow:
         plt.show()
